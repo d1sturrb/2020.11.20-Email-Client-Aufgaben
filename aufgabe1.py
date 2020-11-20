e38a1009529d02a066a88f1755ea436e19cf7e56
d1sturrb <alexander.biber@outlook.com>
2020-11-20 13:20:01 +0100

    Changed

--- aufgabe1.py
+++ aufgabe1.py
@@ -1,32 +1,101 @@
-import smtplib,ssl
+"""
+Inwiefern kann dieses Skript angewendet werden?
+- Wenn man Drittanbietern nicht traut und einen eigenen E-Mail-Client haben möchte
+- Wenn man seine E-Mails durch ein Script generieren will, z.B: täglichen Kalender in eine am morgen versendete E-Mail druckt
+
+Inwiefern könnte das Passwort besser geschützt werden?
+- Passwort verschlüsseln
+
+Haben Sie noch weitere Ansätze, Ihr Skript zu verbessern?
+- Absender verschlüsseln (wenn in betrieblichem Umfeld)
+"""
+
+
+
+""" Imports """
+import email, smtplib,ssl
+from pathlib import Path
 from email.message import EmailMessage
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 """ Attributes """
-sender_email = "testmail@dreampeak.de"
-sender_password = "qg9xfKeuue5jIlDXbe5V"
-receiver_email = "d1sturrb@outlook.com"
+sender_email = "sender@domain.com"
+sender_password = None
+body = "Do you like the picture in the attachment?"
+subject = f"Python-Email | from {sender_email}"
+receiver_email = "receiver@domain.com"
 
-message = """\
-Subject: Hi there
+#Create PW-File
+data_folder = Path("D:/Programme/Microsoft Visual Studio Code/Projects/Python/- DHBW/Semester 1/2020.11.20-Email-Client-Aufgaben")
+file_to_open = data_folder / "password.txt"
+file = open(file_to_open, "w")
 
-This message is sent from Python."""
+#input password
+input_pw = input("Enter password: ")
+file.write(input_pw)
+file.close()
 
+#read password from file
+sender_password = open(file_to_open, "r").read()
+
+
+
+
+
+# Attachment - Picture
+picture_folder = Path("E:/Bilder/Cars/Toyota/Supra/MK4 - A80")
+filename = picture_folder / "Screenshot_20191124-161132.jpg"
+
+
+
+
+
+""" Message """
+message = MIMEMultipart()
+message["From"]= sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.attach(MIMEText(body, "plain")) # Add body to email
+
+
+
+""" Copy & Paste """
+# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email    
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+""" Copy & Paste """
+
+
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
+
+
+
+
+
+
+
+""" Mailserver """
 mailserver = smtplib.SMTP("smtp.strato.de",587)
 mailserver.ehlo()
 mailserver.starttls()
 mailserver.login(sender_email, sender_password)
-mailserver.sendmail(sender_email,receiver_email, str(message))
+mailserver.sendmail(sender_email,receiver_email, str(text))
 mailserver.quit()
-
-#context = ssl.create_default_context()
-
-
-""" Message 
-print("Message")
-
-message = EmailMessage()
-message.set_content("Wohooooo")
-message["To"] = receiver_email
-message["From"] = sender_email
-message["Subject"] = f"The contents of this {sender_email}-Email"
-"""
